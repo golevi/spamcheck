7d553f93ae166e5fecc2d9657e7503ba8c7a6641
Levi Durfee <levi.durfee@gmail.com>
2020-05-24 11:46:56 -0400

    Fix package

--- spamcheck.go
+++ spamcheck.go
@@ -1,130 +1,72 @@
-// Package spamcheck uses Postmark's spam API. The API is free to use. No API
-// key is required. For more information visit https://spamcheck.postmarkapp.com/.
-/*
-	scr := spamcheck.NewRequest("I am a nigerian prince and will give you $1 million")
-	scr = spamcheck.Short
-
-	resp, err := scr.CheckScore()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(resp)
-
-	// {true 7.9 [] }
-*/
 package spamcheck
 
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-const url = "https://spamcheck.postmarkapp.com/filter"
-
-// Options dictates the amount of information returned from the API.
+// Options are either long or short
 type Options string
 
 const (
-	// Long for a full report of processing rules. Default option.
-	Long Options = "long"
-	// Short for a score request.
+	// Short gets the score
 	Short Options = "short"
+	// Long gets a full report
+	Long Options = "long"
 )
 
-// Request is used to send information to the API.
+type rules struct {
+	Score       string `json:"score"`
+	Description string `json:"description"`
+}
+
+// Request is the email and options
 type Request struct {
 	Email   string  `json:"email"`
 	Options Options `json:"options"`
 }
 
-// Response is what is returned from the API.
+// Response is what is received from the api
 type Response struct {
-	Success bool       `json:"success"`
-	Score   string     `json:"score"`
-	Rules   []spamRule `json:"rules"`
-	Report  string     `json:"report"`
+	Score  string  `json:"score"`
+	Report string  `json:"report"`
+	Rules  []rules `json:"rules"`
 }
 
-type spamRule struct {
-	Score       string `json:"score"`
-	Description string `json:"description"`
-}
+// Process the request
+func (r *Request) Process() (*Response, error) {
+	apiResponse := Response{}
 
-// GetReport returns a long response
-func (r *Request) GetReport() {
-
-}
-
-// GetScore gets only the score
-func (r *Request) GetScore() {
-
-}
-
-func process(email, options string) {
-
-}
-
-// NewRequest is a wrapper for creating a new Request.
-func NewRequest(input string) *Request {
-	spamReq := &Request{
-		Email:   input,
-		Options: Long,
-	}
-
-	return spamReq
-}
-
-// CheckScore checks the spam score
-func (s *Request) CheckScore() (Response, error) {
-	if s.Options == "" {
-		s.Options = Long
-	}
-
-	var spamResponse Response
-
-	jsn, err := json.Marshal(s)
+	jsn, err := json.Marshal(&r)
 	if err != nil {
-		return spamResponse, err
+		return &apiResponse, err
 	}
-
-	postData, err := json.Marshal(map[string]string{
-		"email":   s.Email,
-		"options": string(s.Options),
-	})
-
-	resp, err := http.Post(
-		"https://spamcheck.postmarkapp.com/filter",
-		"application/json",
-		bytes.NewBuffer(postData),
-	)
-	defer resp.Body.Close()
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsn))
+	req, err := http.NewRequest("POST", "https://spamcheck.postmarkapp.com/filter", bytes.NewBuffer(jsn))
 	if err != nil {
-		return spamResponse, err
+		return &apiResponse, err
 	}
-	r := &Response{}
-	err = json.NewDecoder(resp.Body).Decode(r)
-	fmt.Println(r.Score)
-
-	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 
 	client := &http.Client{}
-	resp, err = client.Do(req)
+
+	resp, err := client.Do(req)
 	if err != nil {
-		return spamResponse, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return spamResponse, err
+		return &apiResponse, err
 	}
 
-	json.Unmarshal(body, &spamResponse)
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &apiResponse, err
+	}
 
-	return spamResponse, nil
+	// log.Println(string(bodyBytes))
+
+	err = json.Unmarshal(bodyBytes, &apiResponse)
+	if err != nil {
+		return &apiResponse, err
+	}
+
+	return &apiResponse, nil
 }
